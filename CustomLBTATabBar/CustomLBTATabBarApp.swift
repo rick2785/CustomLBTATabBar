6b0215310927be3bd982ba084164b9a32303185b
rick2785 <rjhrabowskie@gmail.com>
2021-01-11 05:18:41 -0600

    Initial Commit

---
+++ CustomLBTATabBar/CustomLBTATabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomLBTATabBarApp.swift
+//  CustomLBTATabBar
+//
+//  Created by RJ Hrabowskie on 1/11/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomLBTATabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
